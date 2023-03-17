onettboots/alpha_frameworks_base

statusbar:  allow user to customize max visible notification icons [1/2]

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,6 @@ public class SystemSettings {
         Settings.System.DISPLAY_COLOR_MODE,
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
+        Settings.System.MAX_VISIBLE_NOTIFICATION_ICONS,
     };
 }

offender6969/android_frameworks_base_ghost

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -93,6 +93,7 @@ public class SystemSettings {
         Settings.System.MEDIA_ARTWORK_BLUR_ENABLED,
         Settings.System.MEDIA_ARTWORK_BLUR_RADIUS,
         Settings.System.HEADS_UP_NOTIFICATIONS_THRESHOLD,
-        Settings.System.NOTIFICATION_HEADERS
+        Settings.System.NOTIFICATION_HEADERS,
+        Settings.System.MAX_VISIBLE_NOTIFICATION_ICONS
     };
 }